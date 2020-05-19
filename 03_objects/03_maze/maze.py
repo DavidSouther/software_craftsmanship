3d684a0a45e7b67863a96da919e5a0ec56ceb704
David Souther <davidsouther@gmail.com>
2020-05-18 21:13:10 -0400

    Prepped maze for ch 3

--- 03_objects/03_maze/maze.py
+++ 03_objects/03_maze/maze.py
@@ -1,6 +1,16 @@
-#/usr/bin/env python3
-
 class Room():
+    """
+    A Room is in a maze.
+
+    It has a brief description. It might have a maze. It has walls in the
+    cardinal directions, and each wall may or may not have a door.
+
+    A room has methods for adding doors in each of the cardinal directions.
+    These methods take the other room to connect this room to, creat a door
+    between them, and sets the door appropriately for both sides. If the other
+    room already had a door there... well, that door is broken and only goes one
+    way now, I guess?
+    """
     def __init__(self, description, key = None):
         self.description = description
         self.key = key
@@ -91,39 +101,46 @@ class Game():
         self.end_room = end_room
         self.player = Player()
         self.player.current_room = self.start_room
+
+    def print_room(self):
+        room = self.player.current_room
+        print("You are in a room.")
+        print(room.description)
+        if room.north_door != None:
+            print("There is a door to the (n)orth.")
+        if room.east_door != None:
+            print("There is a door to the (e)ast.")
+        if room.south_door != None:
+            print("There is a door to the (s)outh.")
+        if room.west_door != None:
+            print("There is a door to the (w)est.")
+    
+    def move_player(self, direction):
+        room = self.player.current_room
+        if direction == "n" and room.north_door != None:
+            self.player.move_through(room.north_door)
+        elif direction == "e" and room.east_door != None:
+            self.player.move_through(room.east_door)
+        elif direction == "w" and room.west_door != None:
+            self.player.move_through(room.west_door)
+        elif direction == "s" and room.south_door != None:
+            self.player.move_through(room.south_door)
+        else:
+            print("No room in that direction.")
     
     def play(self):
         while True:
-            print("You are in a room.")
-            room = self.player.current_room
-            print(room.description)
-            if room.north_door != None:
-                print("There is a door to the (n)orth.")
-            if room.east_door != None:
-                print("There is a door to the (e)ast.")
-            if room.south_door != None:
-                print("There is a door to the (s)outh.")
-            if room.west_door != None:
-                print("There is a door to the (w)est.")
-            print("(q) to quit.")
-            direction = input("Which direction do you go? ")
-            if direction == 'q':
+            self.print_room()
+
+            direction = input("Which direction do you go? (q to quit) ")[0].lower()
+            if direction == "q":
                 break
-            if direction == 'n' and room.north_door != None:
-                self.player.move_through(room.north_door)
-            elif direction == 'e' and room.east_door != None:
-                self.player.move_through(room.east_door)
-            elif direction == 'w' and room.west_door != None:
-                self.player.move_through(room.west_door)
-            elif direction == 's' and room.south_door != None:
-                self.player.move_through(room.south_door)
-            else:
-                print("No room in that direction.")
-                continue
+    
+            self.move_player(direction)
 
             if self.player.current_room == self.end_room:
                 print("You won the treasure!")
-                return
+                break 
 
 blue_key = Key("blue")
 blue_lock = Lock()
