6df3346f34e068a6108db3508bb4f30c88cad47a
David Souther <davidsouther+github@gmail.com>
2020-06-21 10:43:01 -0400

    Template-ish president's pge

--- 06_events/01_web/presidents/web.py
+++ 06_events/01_web/presidents/web.py
@@ -5,6 +5,14 @@ flaskapp = Flask(__name__)
 
 presidents_db = PresidentsDatabase()
 
+def president_page(body):
+    return f"""
+        <html>
+        <head><title>President's Database</title></head>
+        <body>{body}</body>
+        </html>
+        """
+
 @flaskapp.route("/")
 def list_presidents():
     presidents_list = ""
@@ -12,39 +20,21 @@ def list_presidents():
         presidents_list += f"""
             <li><a href="/president/{president.name}">{president}</li>
         """
-
-    return f"""
-    <html>
-    <head><title>President's Database</title></head>
-    <body>
-        <ol>{presidents_list}</ol>
-    </body>
-    </html>
-    """
+    
+    return president_page(f"<ol>{presidents_list}</ol>")
 
 @flaskapp.route("/president/<president_name>")
 def president(president_name):
     for president in presidents_db.presidents:
         if president_name == president.name:
-            return f"""
-            <html>
-            <head><title>President's Database</title></head>
-            <body>
-                <p>
-                    {president.name} served {president.duration.days} days.
-                    He was elected by the {president.party} party.
-                    His vice president was {president.vice_president}.
-                </p>
-            </body>
-            """
-    return f"""
-    <html>
-    <head><title>President's Database</title></head>
-    <body>
-        <p>No president named {president_name}</p>
-    </body>
-    </html>
-    """
+            return president_page(f"""
+            <p>
+                {president.name} served {president.duration.days} days.
+                He was elected by the {president.party} party.
+                His vice president was {president.vice_president}.
+            </p>""")
+    # No president found
+    return f"<p>No president named {president_name}</p>"
 
 if __name__ == "__main__":
     presidents_db.open()
